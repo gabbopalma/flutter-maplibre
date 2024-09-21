b841afc6b366fe604d4aeb7a483594a707c5ad62
Joscha <34318751+josxha@users.noreply.github.com>
2024-09-21 09:37:40 +0200

    Update callbacks_page.dart

--- example/lib/callbacks_page.dart
+++ example/lib/callbacks_page.dart
@@ -1,5 +1,8 @@
+import 'dart:typed_data';
+import 'dart:ui' as ui;
+
 import 'package:flutter/material.dart';
-import 'package:maplibre/maplibre.dart';
+import 'package:flutter/rendering.dart';
 
 @immutable
 class CallbacksPage extends StatefulWidget {
@@ -12,22 +15,64 @@ class CallbacksPage extends StatefulWidget {
 }
 
 class _CallbacksPageState extends State<CallbacksPage> {
+  GlobalKey globalKey = GlobalKey();
+  Uint8List? _pngBytes;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Callbacks')),
-      body: MapLibreMap(
-        options: MapOptions(center: Position(9.17, 47.68)),
-        onMapCreated: (controller) => _print('onMapCreated'),
-        onStyleLoaded: () => _print('onStyleLoadedCallback'),
+      body: Column(
+        children: [
+          OutlinedButton(
+            onPressed: () => setState(() => _pngBytes = null),
+            child: const Text('reset'),
+          ),
+          Expanded(
+            child: _pngBytes == null
+                ? RepaintBoundary(
+                    key: globalKey,
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Icon(
+                            Icons.location_on,
+                            color: Colors.red,
+                            size: 100,
+                          ),
+                          TextButton(
+                            onPressed: _capturePng,
+                            child: const Text(
+                              'Take screenshot',
+                              textDirection: TextDirection.ltr,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                : Image.memory(_pngBytes!),
+          ),
+        ],
       ),
     );
   }
 
-  void _print(String message) {
-    debugPrint('[MapLibreMap] $message');
-    ScaffoldMessenger.of(context)
-      ..clearSnackBars()
-      ..showSnackBar(SnackBar(content: Text(message)));
+  Future<void> _capturePng() async {
+    final sw = Stopwatch()..start();
+    final boundary =
+        globalKey.currentContext!.findRenderObject()! as RenderRepaintBoundary;
+    debugPrint('#1 ${sw.elapsedMicroseconds} us');
+    final image = await boundary.toImage();
+    debugPrint('#2 ${sw.elapsedMicroseconds} us');
+    final byteData = await image.toByteData(format: ui.ImageByteFormat.png);
+    debugPrint('#3 ${sw.elapsedMicroseconds} us');
+    final pngBytes = byteData!.buffer.asUint8List();
+    debugPrint('#4 ${sw.elapsedMicroseconds} us');
+    debugPrint('size: ${pngBytes.lengthInBytes}');
+    setState(() {
+      _pngBytes = pngBytes;
+    });
   }
 }
